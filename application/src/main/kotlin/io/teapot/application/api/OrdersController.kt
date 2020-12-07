c280b7b92ff3a861ac0ed6407d6ebc7792849350
Leonardo C. Fraga <lcfraga@gmail.com>
2020-12-07 03:50:26 +0000

    #10 Adjusted OrdersController to accept lowercase order size

--- application/src/main/kotlin/io/teapot/application/api/OrdersController.kt
+++ application/src/main/kotlin/io/teapot/application/api/OrdersController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 private fun OrderBeverageRequest.toOrderWithoutId() = OrderWithoutId(
     beverage,
     username,
-    size
+    size.toUpperCase()
 )
 
 private fun Order.toOrderResponseBody() = OrderResponseBody(
